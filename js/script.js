e69d82d28629f792481478ad80e4e97c3f657795
opo0123 <dldkdud05@gmail.com>
2026-04-06 18:09:18 +0900

    nintendo-clone

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".visual-Swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
